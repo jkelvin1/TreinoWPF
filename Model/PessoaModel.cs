1f5de433b079ce38c26c0d22fcd24427ae5e4959
John Gabriel <john.gabriel@fatecitapetininga.edu.br>
2021-04-26 16:14:13 -0300

    Treino_INotify

---
+++ Model/PessoaModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace treino_notify.Model
+{
+    public class PessoaModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string nomePropriedade)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nomePropriedade));
+        }
+
+        public PessoaModel(string mdlNome, int mdlCPF)
+        {
+            Nome = mdlNome;
+            Cpf = mdlCPF;
+        }
+
+        private string _nome;
+        private int _cpf;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value;
+                OnPropertyChanged("Nome");
+            }
+        }
+
+        public int Cpf
+        {
+            get { return _cpf; }
+            set
+            {
+                _cpf = value;
+                OnPropertyChanged("Cpf");
+            }
+        }
+
+    }
+}
---
+++ ViewModel/PessoaViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using treino_notify.Command;
+using treino_notify.Model;
+
+namespace treino_notify.ViewModel
+{
+    public class PessoaViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        //private void OnPropertyChanged(string nomePropriedade)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nomePropriedade));
+        //}
+        private void OnPropertyChanged(object sender, string nomePropriedade)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(sender, new PropertyChangedEventArgs(nomePropriedade));
+            }
+        }
+
+        public ObservableCollection<PessoaModel> Pessoa { get; set; }
+        public List<PessoaModel> PessoaNotify { get; set; }
+
+
+        public string Nome { get; set; }
+        public int Cpf { get; set; }
+
+        public ICommand CreatePessoaCommand { get; private set; }
+        public ICommand CreatePessoaNotifyCommand { get; private set; }
+        public ICommand AlteraNomeCommand { get; private set; }
+
+        public PessoaViewModel()
+        {
+            Nome = "String Inicial";
+
+            Pessoa = new ObservableCollection<PessoaModel>();
+            PessoaNotify = new List<PessoaModel>();
+            
+            this.MyCommands();
+        }
+
+        private void MyCommands()
+        {
+            AlteraNomeCommand = new RelayCommand(AlteraNome);
+            CreatePessoaCommand = new RelayCommand(CreatePessoa);
+            CreatePessoaNotifyCommand = new RelayCommand(CreatePessoaNotify);
+
+        }
+
+
+        public void AlteraNome(object objRelayCommand)
+        {
+            Nome = "Maria";
+            OnPropertyChanged(this,"Nome");
+        }
+        public void CreatePessoa(object objRelayCommand)
+        {
+            Pessoa.Add(new PessoaModel("João", 123));
+        }
+        public void CreatePessoaNotify(object objRelayCommand)
+        {
+            PessoaNotify.Add(new PessoaModel("Maria", 456));
+            OnPropertyChanged(this,"Nome");
+            OnPropertyChanged(this,"Cpf");
+        }
+    }
+}
